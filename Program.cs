ec44e6f0e47831e8e8b6e183379ad3c5fbdc8963
awdhah <awdah.almuharwal@gmail.com>
2024-04-15 09:49:10 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Xml.Linq;
+
+var student = new Student(3.00m,"awdhah","Cpeg");
+student.PrintInfo();
+
+public class Student
+{
+    public string Name { get; private set; }
+    public decimal Gpa { get; private set; }
+    public string Major { get; private set; }
+
+
+    public Student (decimal gpa, string name,string major)
+    
+
+    {
+        Major = major;
+        Name = name;
+        Gpa = gpa;
+
+        
+    }
+    public void PrintInfo()
+    {
+
+        Console.WriteLine(" Name: " + Name +
+              "\n Major: " + Major +
+              "\n Gpa: " + Gpa);
+    }
+
+}
+    
+   
+public class GraduatedStudent : Student
+{
+    public GraduatedStudent(decimal gpa, string name, string major) : base(gpa, name, major)
+    {
+    }
+
+    public string ProjectIdea { get; private set; }
+    
+
+
+} 
+
+
+
+
+
